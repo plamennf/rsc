6b2f003af89c98a84a38ecc40e366ae063db8145
plamennf <plamennikolov777@gmail.com>
2022-06-12 19:35:26 +0300

    Release build exe

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 if not exist build mkdir build
 pushd build
 
-set CompilerFlags= -Od -Ob0 -Oi -Zi -FC -Fe:"rsc" -nologo -W3 -fp:fast -fp:except- -Gm- -GR- -EHa- -GS- -Gs9999999
+set CompilerFlags= -O2 -Ob2 -Oi -Zi -FC -Fe:"rsc" -nologo -W3 -fp:fast -fp:except- -Gm- -GR- -EHa- -GS- -Gs9999999
 set Defines= -DBUILD_INTERNAL=1 -DBUILD_SLOW=1 -DBUILD_WIN32 -DCOMPILER_MSVC=1 -D_CRT_SECURE_NO_WARNINGS=1
 set LinkerFlags= -subsystem:console -incremental:no -opt:ref -STACK:0x100000,0x100000
 set Libs= shell32.lib
