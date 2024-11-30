0b84f88b18c8b4efae557e217a0d54bf3ed21a49
plamennf <plamennikolov777@gmail.com>
2024-11-30 19:06:07 +0200

    Added todo.txt

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 if not exist build mkdir build
 pushd build
 
-set CompilerFlags= /Od /Ob0 /MTd /Zi /FC /nologo /Fe:"rsc" /W3
+set CompilerFlags= /O2 /Ob2 /MTd /Zi /FC /nologo /Fe:"rsc" /W3
 REM set Defines= /DBUILD_DEBUG /DOS_WINDOWS /DCOMPILER_MSVC /D_CRT_SECURE_NO_WARNINGS /DUNICODE /D_UNICODE
 set Defines= /DOS_WINDOWS /DCOMPILER_MSVC /D_CRT_SECURE_NO_WARNINGS /DUNICODE /D_UNICODE
 set LinkerFlags= /opt:ref /incremental:no /subsystem:console
